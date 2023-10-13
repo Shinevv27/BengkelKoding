3193888fd3a34779cdea63ea6faea6568ac6bd72
Shinevvs27 <shinn0697@gmail.com>
2023-10-13 11:02:28 +0700

    first commit

---
+++ poliklinik/index.php
@@ -0,0 +1,85 @@
+<?php
+include_once("koneksi.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap Navbar Example</title>
+
+    <!-- Include Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Include Bootstrap JS and Popper.js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js"></script>
+</head>
+<body>
+
+<!-- Your page content goes here -->
+
+<!-- Include Bootstrap JavaScript and jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+<nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      Sistem Informasi Poliklinik
+    </a>
+    <button class="navbar-toggler"
+    type="button" data-bs-toggle="collapse"
+    data-bs-target="#navbarNavDropdown"
+    aria-controls="navbarNavDropdown" aria-expanded="false"
+    aria-label="Toggle navigation">
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="index.php">
+            Home
+          </a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button"
+          data-bs-toggle="dropdown" aria-expanded="false">
+            Data Master
+          </a>
+          <ul class="dropdown-menu">
+            <li>
+              <a class="dropdown-item" href="index.php?page=dokter">
+                Dokter
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="index.php?page=pasien">
+                Pasien
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" 
+          href="index.php?page=periksa">
+            Periksa
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<main role="main" class="container">
+    <?php
+    if (isset($_GET['page'])) {
+    ?>
+        <h2><?php echo ucwords($_GET['page']) ?></h2>
+    <?php
+        include($_GET['page'] . ".php");
+    } else {
+        echo "Selamat Datang di Sistem Informasi Poliklinik";
+    }
+    ?>
+</main>
---
+++ poliklinik/periksa.php
@@ -0,0 +1,174 @@
+<?php
+include_once("koneksi.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap offline -->
+    <link rel="stylesheet" href="assets/css/bootstrap.css"> 
+
+    <!-- Bootstrap Online -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+    rel="stylesheet" 
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+    crossorigin="anonymous">   
+</head>
+<body>
+<div class="container">
+    <hr>    
+    <form class="form row" method="POST" action="" name="myForm" onsubmit="return validate();">
+<div class="form-group mx-sm-3 mb-2">
+    <label for="inputPasien" class="sr-only">Pasien</label>
+    <select class="form-control" name="id_pasien">
+        <?php
+        $selected = '';
+        $pasien = mysqli_query($mysqli, "SELECT * FROM pasien");
+        while ($data = mysqli_fetch_array($pasien)) {
+            if ($data['id'] == $id_pasien) {
+                $selected = 'selected="selected"';
+            } else {
+                $selected = '';
+            }
+        ?>
+            <option value="<?php echo $data['id'] ?>" <?php echo $selected ?>><?php echo $data['nama'] ?></option>
+        <?php
+        }
+        ?>
+    </select>
+</div>
+
+<!-- Form dokter -->
+<div class="form-group mx-sm-3 mb-2">
+    <label for="inputDokter" class="sr-only">Dokter</label>
+    <select class="form-control" name="id_dokter">
+        <?php
+        $selected = '';
+        $dokter = mysqli_query($mysqli, "SELECT * FROM dokter");
+        while ($data = mysqli_fetch_array($dokter)) {
+            if ($data['id'] == $id_dokter) {
+                $selected = 'selected="selected"';
+            } else {
+                $selected = '';
+            }
+        ?>
+            <option value="<?php echo $data['id'] ?>" <?php echo $selected ?>><?php echo $data['nama'] ?></option>
+        <?php
+        }
+        ?>
+    </select>
+</div>
+
+<!-- Form tanggal periksa -->
+<div class="form-group mx-sm-3 mb-2">
+    <label for="inputTanggalPeriksa" class="sr-only">Tanggal Periksa</label>
+    <input type="date" class="form-control" name="tgl_periksa" value="<?php echo $tgl_periksa; ?>">
+</div>
+
+<!-- Form tanggal periksa -->
+<div class="form-group mx-sm-3 mb-2">
+    <label for="inputCatatan" class="sr-only">Catatan</label>
+    <input type="text" class="form-control" name="catatan" id="inputCatatan" placeholder="Catatan" value="<?php echo $catatan; ?>">
+</div>
+
+
+<?php
+$result = mysqli_query($mysqli, "SELECT pr.*,d.nama as 'nama_dokter', p.nama as 'nama_pasien' FROM periksa pr LEFT JOIN dokter d ON (pr.id_dokter=d.id) LEFT JOIN pasien p ON (pr.id_pasien=p.id) ORDER BY pr.tgl_periksa DESC");
+$no = 1;
+while ($data = mysqli_fetch_array($result)) {
+?>
+    <tr>
+        <td><?php echo $no++ ?></td>
+        <td><?php echo $data['nama_pasien'] ?></td>
+        <td><?php echo $data['nama_dokter'] ?></td>
+        <td><?php echo $data['tgl_periksa'] ?></td>
+        <td><?php echo $data['catatan'] ?></td>
+        <td>
+            <a class="btn btn-success rounded-pill px-3" 
+            href="index.php?page=periksa&id=<?php echo $data['id'] ?>">
+            Ubah</a>
+            <a class= "btn btn-danger rounded-pill px-3" 
+            href="index.php?page=periksa&id=<?php echo $data['id'] ?>&aksi=hapus">Hapus</a>
+        </td>
+    </tr>
+<?php
+}
+?>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Pasien</th>
+                <th scope="col">Dokter</th>
+                <th scope="col">Tanggal Periksa</th>
+                <th scope="col">Catatan</th>
+                <th scope="col">Aksi</th>
+            </tr>
+        </thead>
+        
+        <tbody>
+            <?php
+            $result = mysqli_query(
+                $mysqli,"SELECT * FROM periksa"
+            );
+            $no = 1;
+            while ($data = mysqli_fetch_array($result)) {
+            ?>
+                <tr>
+                    <th scope="row"><?php echo $no++ ?></th>
+                    <td><?php echo $data['id_pasien'] ?></td>
+                    <td><?php echo $data['id_dokter'] ?></td>
+                    <td><?php echo $data['tgl_periksa'] ?></td>
+                    <td><?php echo $data['catatan'] ?></td>
+                    <td>
+                        <a class="btn btn-info rounded-pill px-3" 
+                        href="pasien.php?id=<?php echo $data['id'] ?>">Ubah
+                        </a>
+                        <a class="btn btn-danger rounded-pill px-3" 
+                        href="pasien.php?id=<?php echo $data['id'] ?>&aksi=hapus">Hapus
+                        </a>
+                    </td>
+                </tr>
+            <?php
+            }
+            ?>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
+<?php
+if (isset($_POST['simpan'])) {
+    if (isset($_POST['id'])) {
+        // Corrected table name from 'dokter' to 'pasien'
+        $ubah = mysqli_query($mysqli, "UPDATE pasien SET 
+                                        nama = '" . $_POST['nama'] . "',
+                                        alamat = '" . $_POST['alamat'] . "',
+                                        no_hp = '" . $_POST['no_hp'] . "'
+                                        WHERE
+                                        id = '" . $_POST['id'] . "'");
+    } else {
+        $tambah = mysqli_query($mysqli, "INSERT INTO pasien(nama,alamat,no_hp) 
+                                        VALUES ( 
+                                            '" . $_POST['nama'] . "',
+                                            '" . $_POST['alamat'] . "',
+                                            '" . $_POST['no_hp'] . "'
+                                            )");
+    }
+
+    echo "<script> 
+            document.location='pasien.php';
+            </script>";
+}
+
+if (isset($_GET['aksi'])) {
+    if ($_GET['aksi'] == 'hapus') {
+        $hapus = mysqli_query($mysqli, "DELETE FROM pasien WHERE id = '" . $_GET['id'] . "'");
+    }
+    echo "<script> 
+            document.location='pasien.php';
+            </script>";
+}
+?>
