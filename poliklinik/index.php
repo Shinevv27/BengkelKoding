2a20410a3c32ed892255a8a9f30cd920720d6142
Shinevv27 <shinn0697@gmail.com>
2023-10-26 22:01:40 +0700

    hai

--- poliklinik/index.php
+++ poliklinik/index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head> 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistem Informasi Poliklinik</title>
+    <title>Sistem Informasi Poliklinik hai</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
